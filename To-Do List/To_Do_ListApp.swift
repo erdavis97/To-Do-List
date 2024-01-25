b8e87e3eff61fa7a6ba6007a589c4eae7ae2fa63
Ethan Davis <edavis@bsd220.org>
2024-01-25 09:27:38 -0600

    Initial Commit

---
+++ To-Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To-Do List
+//
+//  Created by Ethan Davis on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
